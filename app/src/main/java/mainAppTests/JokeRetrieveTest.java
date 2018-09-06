ddd4f63c15a389c3eab0ac5de3be7bf21d943894
nabishop <35745821+nabishop@users.noreply.github.com>
2018-09-05 17:53:17 -0700

    server up and running

--- app/src/main/java/mainAppTests/JokeRetrieveTest.java
+++ app/src/main/java/mainAppTests/JokeRetrieveTest.java
@@ -1,6 +1,33 @@
 package mainAppTests;
 
+import com.udacity.gradle.builditbigger.EndpointsAsyncTask;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.concurrent.CountDownLatch;
+
+@RunWith(JUnit4.class)
 public class JokeRetrieveTest {
-
+    @Test
+    public void testJokeLoading() {
+        final CountDownLatch countDownLatch = new CountDownLatch(1);
+        new EndpointsAsyncTask(new EndpointsAsyncTask.JokeCaller() {
+            @Override
+            public void jokeReady(String joke) {
+                if (joke == null || joke.isEmpty()) {
+                    Assert.fail();
+                }
+                countDownLatch.countDown();
+            }
+        }).execute();
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            Assert.fail();
+            e.printStackTrace();
+        }
+    }
 }
